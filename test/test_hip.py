a50292301d1ef88847222b93c4b5c846b6da29e9
George Hotz <geohot@gmail.com>
2023-11-25 19:53:38 -0800

    expectedFailureIf CI

--- test/test_hip.py
+++ test/test_hip.py
@@ -1,10 +1,19 @@
 import unittest
+import os
 import ctypes
 import gpuctypes.hip as hip
 from ctypes_helpers import to_char_p_p
 
+def expectedFailureIf(condition):
+  def wrapper(func):
+    if condition: return unittest.expectedFailure(func)
+    else: return func
+  return wrapper
+
+CI = os.getenv("CI", "") != ""
+
 def check(status):
-  if status != 0: raise RuntimeError(f"HIP Error {status}, {ctypes.string_at(hip.hipGetErrorString(status))}")
+  if status != 0: raise RuntimeError(f"HIP Error {status}, {ctypes.string_at(hip.hipGetErrorString(status)).decode()}")
 
 def get_hip_bytes(arg, get_sz, get_str) -> bytes:
   sz = ctypes.c_size_t()
@@ -14,18 +23,26 @@ def get_hip_bytes(arg, get_sz, get_str) -> bytes:
   return ctypes.string_at(mstr, size=sz.value)
 
 class TestHIP(unittest.TestCase):
+  def test_has_methods(self):
+    assert hip.hipMalloc is not None
+    assert hip.hiprtcCompileProgram is not None
+    assert hip.hipGetDeviceProperties is not None
+
+  @expectedFailureIf(CI)
   def test_malloc(self):
     ptr = ctypes.c_void_p()
     check(hip.hipMalloc(ctypes.byref(ptr), 100))
     assert ptr.value != 0
     check(hip.hipFree(ptr))
 
+  @expectedFailureIf(CI)
   def test_get_device_properties(self) -> hip.hipDeviceProp_t:
     device_properties = hip.hipDeviceProp_t()
     check(hip.hipGetDeviceProperties(device_properties, 0))
     print(device_properties.gcnArchName)
     return device_properties
 
+  @expectedFailureIf(CI)
   def test_compile_fail(self):
     prg = "void test() { {"
     prog = hip.hiprtcProgram()
@@ -37,6 +54,7 @@ class TestHIP(unittest.TestCase):
     assert len(log) > 10
     print(log)
 
+  @expectedFailureIf(CI)
   def test_compile(self):
     prg = "void test() { }"
     prog = hip.hiprtcProgram()
