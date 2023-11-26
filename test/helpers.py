9dc8fc6836274f15703f9f6b7c929d21aa960e29
George Hotz <george@comma.ai>
2023-11-25 21:18:24 -0800

    more opencl tests

--- test/helpers.py
+++ test/helpers.py
@@ -1,11 +1,6 @@
 from typing import List
 import unittest, os, ctypes
 
-def to_char_p_p(options: List[str]):
-  c_options = (ctypes.POINTER(ctypes.c_char) * len(options))()
-  c_options[:] = [ctypes.cast(ctypes.create_string_buffer(o.encode("utf-8")), ctypes.POINTER(ctypes.c_char)) for o in options]
-  return c_options
-
 def expectedFailureIf(condition):
   def wrapper(func):
     if condition: return unittest.expectedFailure(func)
@@ -13,10 +8,17 @@ def expectedFailureIf(condition):
   return wrapper
 CI = os.getenv("CI", "") != ""
 
+# helpers for RTC
+
 def get_bytes(arg, get_sz, get_str, check) -> bytes:
   check(get_sz(arg, ctypes.byref(sz := ctypes.c_size_t())))
   check(get_str(arg, mstr := ctypes.create_string_buffer(sz.value)))
   return ctypes.string_at(mstr, size=sz.value)
+
+def to_char_p_p(options: List[str]):
+  c_options = (ctypes.POINTER(ctypes.c_char) * len(options))()
+  c_options[:] = [ctypes.cast(ctypes.create_string_buffer(o.encode("utf-8")), ctypes.POINTER(ctypes.c_char)) for o in options]
+  return c_options
 
 def cuda_compile(prg, options, f, check):
   check(f.create(ctypes.pointer(prog := f.new()), prg.encode(), "<null>".encode(), 0, None, None))
