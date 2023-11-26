6fc8820fe7bdae629687e575204b7904b808a2f2
George Hotz <geohot@gmail.com>
2023-11-25 19:05:29 -0800

    include CL_DEVICE_TYPE_DEFAULT in the py

--- generate_opencl.sh
+++ generate_opencl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-clang2py /usr/include/CL/cl.h -o gpuctypes/opencl.py -l /usr/lib/x86_64-linux-gnu/libOpenCL.so
+clang2py /usr/include/CL/cl.h -o gpuctypes/opencl.py -l /usr/lib/x86_64-linux-gnu/libOpenCL.so -k cdefstum
 grep FIXME_STUB gpuctypes/opencl.py || true
